22e9887009a1c1d30c41010e5c204768d2424d10
Arpit Agarwal <arpit.agarwal@lenskart.in>
2026-05-27 00:58:15 +0530

    Initial Xcode project

---
+++ Wardlume/WardlumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardlumeApp.swift
+//  Wardlume
+//
+//  Created by Arpit  on 27/05/26.
+//
+
+import SwiftUI
+
+@main
+struct WardlumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
